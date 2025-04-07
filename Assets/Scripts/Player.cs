2851cc8f3f7fe28761303e2bf07dc53f1d11ab5d
Hayunz <khy4450@naver.com>
2025-04-07 21:06:01 +0900

    유니티 프로젝트 세팅

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    public Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        inputVec.x=Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+
+    private void FixedUpdate()
+    {
+            Vector2 nextVec = inputVec.normalized * Time.fixedDeltaTime*speed;
+            rigid.MovePosition (rigid.position + nextVec);
+            
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriteRenderer.flipX = inputVec.x < 0;
+            Debug.Log("dd");
+        }
+    }
+
+
+
+    
+}
