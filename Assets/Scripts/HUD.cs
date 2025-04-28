df6695f201709b82fc94fe8af9188b452f5b90de
Hayunz <khy4450@naver.com>
2025-04-28 12:13:17 +0900

    [하윤] 킬수, 타이머, 체력 게이지 UI 추가 및 연동

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -26,15 +26,24 @@ public class HUD : MonoBehaviour
                 break;
 
             case InfoType.Level:
+                myText.text = string.Format("Lv. {0:F0}", GameManager.instance.level); //인자 값의 문자열이 들어갈 자리를 {순번} 형태로 작성
                 break;
 
             case InfoType.Kill:
+                myText.text = string.Format("{0:F0}", GameManager.instance.kill); //인자 값의 문자열이 들어갈 자리를 {순번} 형태로 작성
                 break;
 
             case InfoType.Time:
+                float remainTime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remainTime / 60);
+                int sec = Mathf.FloorToInt(remainTime % 60);
+                myText.text = string.Format("{0:D2}:{1:D2}", min, sec);
                 break;
 
             case InfoType.Health:
+                float curHealth = GameManager.instance.health;
+                float maxHealth = GameManager.instance.maxHealth;
+                mySlider.value = curHealth / maxHealth;
                 break;
         }
     }
